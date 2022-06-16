10901e17a61e0e6cc155e4785ad957dbee934633
sailer <robin.guov@gmail.com>
2022-06-16 17:36:04 +0800

    fix

--- Sources/WRNavigationBar/WRDefaultBackButton.swift
+++ Sources/WRNavigationBar/WRDefaultBackButton.swift
@@ -44,7 +44,7 @@ struct WRDefaultBackButton: View {
 //            let path = Bundle.module.path(forResource: named, ofType: "png")
             return Image(named!, bundle: Bundle.module)
         } else {
-            let image = Image(isBlack ? "back_arrow@2x" : "back_arrow_white", bundle: Bundle.module)
+            let image = Image(isBlack ? "back_arrow" : "back_arrow_white", bundle: Bundle.module)
             return image
         }
     }
